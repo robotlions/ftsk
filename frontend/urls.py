9456e57b9f3f7d6e13b04295e8f4b0204aaae938
robotlions <chadmusick@gmail.com>
2021-04-30 23:33:10 -0400

    initial commit

---
+++ frontend/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path('<path:resource>/', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='index'),
+]
