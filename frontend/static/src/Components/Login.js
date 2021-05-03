509962c85a88fd4560bf1b6a3714b1879bff7121
robotlions <chadmusick@gmail.com>
2021-05-03 13:17:26 -0400

    removes uncontrolled state from login

--- frontend/static/src/Components/Login.js
+++ frontend/static/src/Components/Login.js
@@ -1,109 +1,114 @@
-import React, {Component} from 'react'
-import Cookies from 'js-cookie'
-import '../App.css';
+import React, { Component } from "react";
+import Cookies from "js-cookie";
+import "../App.css";
 
 class Login extends Component {
-  constructor (props){
-        super(props);
-        this.state = {
-          isLoggedIn: !!Cookies.get('Authorization'),
-          username: "",
-          email: "",
-          password: "",
-          data: [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: !!Cookies.get("Authorization"),
+      username: "",
+      email: "",
+      password: "",
+      data: [],
+    };
     this.handleInput = this.handleInput.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
     this.reset = this.reset.bind(this);
-      }
+  }
 
+  reset() {
+    this.setState({ username: "" });
+    this.setState({ password: "" });
+    window.location.reload();
+  }
 
-
-
-reset(){
-  this.setState({usernamelog: ""})
-  this.setState({passlog: ""})
-  this.setState({username: ""})
-  this.setState({password: ""})
-  window.location.reload();
-}
-
-
-  async handleLogin(e){
+  async handleLogin(e) {
     e.preventDefault();
     let obj = {
-      username: this.state.usernamelog,
-      password: this.state.passlog,
-    }
+      username: this.state.username,
+      password: this.state.password,
+    };
     const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFToken': Cookies.get('csrftoken'),
-        },
-        body: JSON.stringify(obj),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+      body: JSON.stringify(obj),
     };
     const handleError = (err) => console.warn(err);
-    const response = await fetch('/rest-auth/login/', options);
+    const response = await fetch("/rest-auth/login/", options);
     const data = await response.json().catch(handleError);
-    if(data.key) {
-    Cookies.set('Authorization', `Token ${data.key}`);
-    alert('Logged in!')
-    this.reset();
+    if (data.key) {
+      Cookies.set("Authorization", `Token ${data.key}`);
+      alert("Logged in!");
     }
-
   }
 
+  handleInput(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-handleInput(event){
-          this.setState({[event.target.name]: event.target.value});
-        }
+  handleLogout() {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+    };
+    fetch("/rest-auth/logout/", options)
+      .then((response) => response.json())
+      .then((response) => this.setState({ data: response }));
+    Cookies.remove("Authorization");
+    Cookies.remove("sessionid");
+    localStorage.clear();
+  }
 
-
-
-
-handleLogout(){
-      const options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-CSRFToken': Cookies.get('csrftoken'),
-            },
-        };
-        fetch("/rest-auth/logout/", options)
-          .then(response => response.json())
-          .then(response => this.setState({data: response}));
-          Cookies.remove('Authorization');
-          Cookies.remove('sessionid');
-          localStorage.clear()
-
-
-    }
-
-
-
-  render(){
-
-
-// const loginForm = <form onSubmit={(e) => this.handleLogin(e, this.state)}>
-const loginForm = <form onSubmit={(e) => this.handleLogin(e, this.state)}>
-      <input className="input-group form-control" type="text" placeholder="username" name="usernamelog" value={this.state.usernamelog} onChange={this.handleInput}/>
-      <input className="input-group form-control" type="password" placeholder="password" name="passlog" value={this.state.passlog} onChange={this.handleInput}/>
-      <button className="logButton" onClick={(e)=>this.handleLogin(e, this.state)} type="submit">Log In</button>
+  render() {
+    const loginForm = (
+      <form onSubmit={(e) => this.handleLogin(e, this.state)}>
+        <input
+          className="input-group form-control"
+          type="text"
+          placeholder="username"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInput}
+        />
+        <input
+          className="input-group form-control"
+          type="password"
+          placeholder="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInput}
+        />
+        <button
+          className="logButton"
+          onClick={(e) => this.handleLogin(e, this.state)}
+          type="submit"
+        >
+          Log In
+        </button>
       </form>
+    );
 
+    const logOutForm = (
+      <form onSubmit={(e) => this.handleLogout(e, this.state)}>
+        <button className="logButton" type="submit">
+          Log Out
+        </button>
+      </form>
+    );
 
-const logOutForm = <form onSubmit={(e) => this.handleLogout(e, this.state)}>
-<button className="logButton" type="submit">Log Out</button></form>
-
-
-
-      return(
-        <div>
-            {loginForm}
-            {logOutForm}
-        </div>
-      );
-    }
+    return (
+      <div>
+        {loginForm}
+        {logOutForm}
+      </div>
+    );
   }
-  export default Login
+}
+export default Login;
