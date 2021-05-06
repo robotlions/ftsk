3443bed317a266043ff40c07daca0eda9ab98a2c
robotlions <chadmusick@gmail.com>
2021-05-05 20:42:20 -0400

    adds location and time info

--- frontend/static/src/Components/Schedule.js
+++ frontend/static/src/Components/Schedule.js
@@ -14,6 +14,7 @@ class Schedule extends Component {
     <div>
         <h4 style={{marginTop: "3vh"}}>Where we are today:</h4>
         <p>Location Information</p>
+        <p>Time - Lunch or Dinner</p>
         <h4>Map</h4>
         <p>Theoretically, right here will be a Google map to the current location, which can be updated.</p>
     </div>
